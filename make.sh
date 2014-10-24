807dbdc1e6bec9b1c0714523397b7711c5812316
Kristaps Austers <kristaps@austers.lv>
2014-10-25 00:27:03 +0300

    Function names

--- make.sh
+++ make.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-function init {
+function init_vars {
 	dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # set working directory
 	src=$dir/apps
 	out=$dir/upload
 	deb=$out/deb
 }
 
-function purge {
+function purge_out {
 	cd $dir
 	find . -name '*.DS_Store' -type f -delete
 	find . -name '.deb' -type f -delete
 	rm -rf $out/*
 }
 
-function copy_repo {
+function copy_repo_to_out {
 	cd $dir/repo
 	cp * $out
 }
 
-function compile {
+function make_apps {
 	cd $src
 	for SRC in `find . -type d -mindepth 1 -maxdepth 1`
 	#for SRC in `ls -d */`
@@ -28,28 +28,28 @@ function compile {
 	done
 }
 
-function move {
+function move_apps_to_out {
 	mkdir $deb
 	mv *.deb $deb
 	cd $out
 }
 
-function merge {
+function merge_apps {
 	dpkg-scanpackages -m . >Packages
 	gzip -9 -f Packages
 }
 
-function clean {
+function clean_vars {
 	unset dir
 	unset src
 	unset out
 	unset deb
 }
 
-init
-purge
-copy_repo
-compile
-move
-merge
-clean
+init_vars
+purge_out
+copy_repo_to_out
+make_apps
+move_apps_to_out
+merge_apps
+clean_vars
