67e973d885d05611537d837466572fe97a972cae
Kristaps Austers <kristaps@austers.lv>
2014-10-24 23:51:28 +0300

    Split make script into functions

--- make.sh
+++ make.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # set working directory
+function init {
+	dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # set working directory
+}
 
-cd $dir
-find . -name '*.DS_Store' -type f -delete
-find . -name '.deb' -type f -delete
-rm -f $dir/upload/Packages.gz
-rm -rf $dir/upload/deb/
+function clean {
+	cd $dir
+	find . -name '*.DS_Store' -type f -delete
+	find . -name '.deb' -type f -delete
+	rm -f $dir/upload/Packages.gz
+	rm -rf $dir/upload/deb/
+}
 
-cd $dir/apps
-for SRC in `find . -type d -mindepth 1 -maxdepth 1`
-#for SRC in `ls -d */`
-do
-	dpkg-deb -b -Zgzip ${SRC}
-done
-mkdir $dir/upload/deb/
-mv *.deb $dir/upload/deb/
-cd $dir/upload
-dpkg-scanpackages -m . /dev/null >Packages
-gzip -9 -f Packages
+function compile {
+	cd $dir/apps
+	for SRC in `find . -type d -mindepth 1 -maxdepth 1`
+	#for SRC in `ls -d */`
+	do
+		dpkg-deb -b -Zgzip ${SRC}
+	done
+}
+
+function move {
+	mkdir $dir/upload/deb/
+	mv *.deb $dir/upload/deb/
+	cd $dir/upload
+}
+
+function merge {
+	dpkg-scanpackages -m . /dev/null >Packages
+	gzip -9 -f Packages
+}
+
+init
+clean
+compile
+move
+merge
