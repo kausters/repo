13b1c74d1fc6e8c9b6b7c2de20f363ce9f22e8b7
Kristaps Austers <kristaps@austers.lv>
2014-10-25 01:15:11 +0300

    Generate standard package names

--- make.sh
+++ make.sh
@@ -10,15 +10,14 @@ function reset_out {
 	find $src -name '*.DS_Store' -type f -delete
 	rm -rf $out/*
 	cp $dir/repo/* $out
+	mkdir -p $out/deb
 }
 
 function make_apps {
 	for app in `find $src -type d -mindepth 1 -maxdepth 1`
 	do
-		dpkg-deb -b -Zgzip $app 2>/dev/null
+		dpkg-deb -b -Zgzip $app $out/deb 2>/dev/null
 	done
-	mkdir -p $out/deb
-	mv $src/*.deb $_
 }
 
 function composite {
